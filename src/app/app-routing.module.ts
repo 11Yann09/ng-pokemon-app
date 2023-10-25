e4e2dbde97d209992e58858c8c7fe38617dcef75
Demaux Yann <110088864+11Yann09@users.noreply.github.com>
2023-10-25 10:40:08 +0200

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
+import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pokemon', component: ListPokemonComponent },
+  { path: 'pokemon/:id', component: DetailPokemonComponent },
+  { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { POKEMONS } from "./mock-pokemon-list";
-import { Pokemon } from "./pokemon";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   // Modifier le template pour affichier le message suivant : Liste de Pokémons
-  template: `<h1>Liste de Pokémons !</h1>
-    <p>Ceci est un paragraphe</p>
-    <p>Ceci est un paragraphe</p>
-    <p>Ceci est un paragraphe</p> `,
+  templateUrl: "./app.component.html",
 })
-
-export class AppComponent implements OnInit {
-  // Charger la liste des 12 pokemons dans le composant
-  pokemonList: Pokemon[] = POKEMONS;
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
-  }
-
-  // Passer en paramètre de la méthode selectPokemon un pokemon issue du mock de pokemon créee précédemment
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`Vous avez choisi ${pokemon.name} !`);
-  }
-}
+export class AppComponent {}
