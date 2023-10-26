a2bf67aac1905e557aad47d64b190d19137dc446
Demaux Yann <110088864+11Yann09@users.noreply.github.com>
2023-10-26 10:50:49 +0200

    Modification du projet avec modules services

---
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+import { Pokemon } from "../pokemon";
+import { Router } from "@angular/router";
+import { PokemonService } from "../pokemon.service";
+
+@Component({
+  selector: "app-list-pokemon",
+  templateUrl: "./list-pokemon.component.html",
+})
+export class ListPokemonComponent implements OnInit {
+  pokemonList: Pokemon[];
+
+  constructor(private router: Router, private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.pokemonList = this.pokemonService.getPokemonList();
+  }
+
+  goToPokemon(pokemon: Pokemon) {
+    this.router.navigate(["/pokemon", pokemon.id]);
+  }
+}
